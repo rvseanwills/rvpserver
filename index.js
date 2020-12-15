d1770ad0dae2380e82ab7cb5f681c4023cc69cab
rvseanwills <rvb.seanwills@gmail.com>
2020-12-15 19:56:43 +0000

    cors 16plus

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Request-Headers", 'Origin,X-Requested-With,Accept, Content-Type');
+    res.header("Access-Control-Request-Headers", 'Origin,X-Requested-With,Accept,Content-Type');
     next();
 });
 
