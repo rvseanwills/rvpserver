2da30c45a34dcd5b32881c8b965f51d7b2120923
rvseanwills <rvb.seanwills@gmail.com>
2020-12-15 19:48:21 +0000

    cors 15 nvm i wasn't

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Request-Headers", 'Origin,X-Requested-With,Accept, application/json');
+    res.header("Access-Control-Request-Headers", 'Origin,X-Requested-With,Accept, content-type, application/json');
     next();
 });
 
