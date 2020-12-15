285221074e8725525a0c73c2f9993a6fb75174a4
rvseanwills <rvb.seanwills@gmail.com>
2020-12-15 06:10:05 +0000

    cors fix 4

--- index.js
+++ index.js
@@ -27,8 +27,13 @@ mongoose
     console.log({ database_error: err });
   });
 // db configuaration ends here
-//registering cors
-app.use(cors({origin: '*'}));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 //configure body parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
