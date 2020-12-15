3e6f7e7762f2142b9a3cbc6258d702d622c496d4
rvseanwills <rvb.seanwills@gmail.com>
2020-12-15 20:09:33 +0000

    cors 17

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Request-Headers", 'Origin,X-Requested-With,Accept,Content-Type,content-type');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Accept,Content-Type,content-type');
     next();
 });
 
